d15f833cc99d62e5d9287ef6df8047da9776da65
Arthur El Malawi <70762222+ArthurElm@users.noreply.github.com>
2020-12-02 12:44:00 +0100

    :construction: UPDATE

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,41 @@
-const { src, dest, symlink, parallel, watch } = require('gulp');
-const del = require('del');
-const gulpSass = require('gulp-sass');
+const { src, dest, parallel, watch } = require('gulp');
+const gulpSass = require('gulp-sass')
 const browserSync = require('browser-sync').create();
 
 
-// Browser Sync
-function browser(){
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  watch("*.html").on('change', browserSync.reload);
+// recharge le navigateur
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch('*.html').on("change", browserSync.reload);
 }
 
-// Sass (scss -> css)
-function sass(){
-  return src('./sass/*.scss')
-  .pipe(gulpsass())
-  .pipe(dest('./css/'))
-  .pipe(browserSync.strem());
-}
-
-
-// Watch Sass
-function watcher(){
-  watch('./sass/*.scss', sass)
-}
-
-// Src + Dest
-function srcExemple() {
-    return src('./index.html')
-        .pipe(dest('dossier1/'))
-}
-
-// Clean
-function clean() {
-    return del('dossier1');
-}
-
-// LinkExemple
-function linkExemple() {
-    return src('./test.html')
-        .pipe(symlink('dossier1'));
-}
-
-// Parallel
-// function css(log) {
-//     console.log('Tâche 1, exemple de compilation')
-//     log();
-// }
-
-// function sass(log) {
-//     console.log('Tâche 2, exemple de minification')
-//     log();
-// }
-
+// scss to css
 function sass() {
-    return src('./sass/*.scss')
+    return src('./sass/import.scss')
         .pipe(gulpSass())
         .pipe(dest('./css/'))
+        .pipe(browserSync.stream())
 }
 
+// Watch sass
+function watcher(done) {
+    watch('./sass/', sass)
+    done();
+}
+
+
+
+
+// lancer la fonction browser et watcher en parallele
+
+
 module.exports = {
-    srcExemple,
-    clean,
-    linkExemple,
-    sass,
-    watcher
     // build: parallel(css, sass)
-    // build: parallel(browser, watcher)
+    sass,
+    watcher,
+    browser: parallel(browser, watcher)
 }
